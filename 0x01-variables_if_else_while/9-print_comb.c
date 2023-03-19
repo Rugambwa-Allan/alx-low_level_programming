49e1330c68ff94cd11fb8d3cb98dfd24130e58a7
myUsername <myemail.gmail.com>
2023-03-19 05:58:17 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-
-	putchar(i + '0');
-	putchar(',');
-	putchar(' ');
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-return (0);
-
-
-
+	putchar('\n');
+	return (0);
 }
