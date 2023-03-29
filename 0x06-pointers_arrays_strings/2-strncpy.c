80068ea41b74ce8ee03454c0fa6aef9a1ba28e52
myUsername <myemail.gmail.com>
2023-03-29 16:46:48 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * *_strncpy - See description
- * @dest: char type string
- * @src: char type string
- * @n: number of strings
- * Description: Copy the string pointed to by pointer `src` to
- * the buffer pointed to by `dest`
- * Return: Pointer to `dest`
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-
-
 char *_strncpy(char *dest, char *src, int n)
 {
+	int j;
 
-	int i;
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
-	for (i = 0; i <= n ; i++)
-		dest[i] = src[i];
 	return (dest);
 }
