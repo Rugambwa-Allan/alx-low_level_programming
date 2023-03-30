3235b49c2b1c510e9b38a63726a89dbc637f6c6d
myUsername <myemail.gmail.com>
2023-03-30 10:36:14 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,26 @@
+#include "main.h"
 
+/**
+ * *_strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to be apprend
+ * Return: concatenated string
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i < n ; i++)
+		dest[destlen + 1] = src[i];
+	return (dest);
+
+}
