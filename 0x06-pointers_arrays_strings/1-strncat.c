7629f765b92d47a5cbbb1082f4b545dfe0b774b4
myUsername <myemail.gmail.com>
2023-03-30 10:45:51 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,7 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0 ; src[i] != '\0' ; i++)
 		srclen++;
 	for (i = 0 ; i < n ; i++)
-		dest[destlen + 1] = src[i];
+		dest[destlen + i] = src[i];
 	return (dest);
 
 }
