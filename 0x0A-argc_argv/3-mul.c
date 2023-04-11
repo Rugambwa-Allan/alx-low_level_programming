c884f570b007e16bc9cba2489bd4574720e07a02
myUsername <myemail.gmail.com>
2023-04-10 17:59:07 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - prints arguments that multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 
@@ -11,11 +20,11 @@ if (argc < 3 || argc > 3)
 printf("Error\n");
 return (1);
 }
-else 
+else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-mul = num1 * num2;
+mul = num1  *num2;
 printf("%d\n", mul);
 }
 return (0);
