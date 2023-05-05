88a1b093edbd1a0434218f5d69269b79e3d19808
myUsername <myemail@gmail.com>
2023-05-05 15:47:18 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
  * print_listint - prints all  elements of a linked list
  * @h: linked list of type listint_t to print
  *
- * Return: number of nodes
+ * Return: num
  */
 size_t print_listint(const listint_t *h)
 {
@@ -19,4 +19,3 @@ size_t print_listint(const listint_t *h)
 
 	return (num);
 }
-
