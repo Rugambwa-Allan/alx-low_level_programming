55c8cab62f77b2fbae36ae5b74a20a2d9da041b7
myUsername <myemail@gmail.com>
2023-05-20 11:14:43 -0700

    function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find loop in a linked list
+ * @head: linked list to search
+ *
+ * Return: address of node where the loop starts, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *down = head;
+	listint_t *up = head;
+
+	if (!head)
+		return (NULL);
+
+	while (down && up && up->next)
+	{
+		up = up->next->next;
+		down = down->next;
+		if (up == down)
+		{
+			down = head;
+			while (down != up)
+			{
+				down = down->next;
+				up = down->next;
+			}
+			return (up);
+		}
+	}
+
+	return (NULL);
+}
